8b9d66ab862b73ea67634a005fe0ff78db339f08
landesprit <julien.bataille40@gmail.com>
2024-01-31 13:49:22 +0100

    MAJ

--- scripts/class.js
+++ scripts/class.js
@@ -7,13 +7,14 @@ function Line(speed,dir,width,type){
 }
 
 //Objet(Enemies/Platfomes) caractérisé par ses positions x,y et par une "Line"
-function Obj(x,y,line){ 
+class Obj{
+	constructor(x, y, line){
    	this.x=x; 
     this.y=y; 
-	this.line=line;
+	this.line=line;} 
 	
 	//Fonction qui determine si l'objet et en collision avec le frog	
-	this.inCollision=function(frog){
+	inCollision(frog){
 		return frog.x+25>=this.x && frog.x+5<=(this.x+this.line.width)
 		// if(frog.x+25>=this.x && frog.x+5<=(this.x+this.line.width)){
 		// 	return true;
@@ -23,7 +24,7 @@ function Obj(x,y,line){
 		// }
 	}
 	//Fonction qui déplace l'object selon sa vitesse & direction	
-	this.move=function(){
+	move(){
 		this.line.dir == 1
 		? this.x = this.x >650
 			? - (this.line.width*3) 
@@ -31,6 +32,7 @@ function Obj(x,y,line){
 		: this.x = this.x < - this.line.width 
 			? 650+(this.line.width*3) 
 			: this.x + (this.line.speed*this.line.dir);
+
 	// 	if(this.line.dir==1){
 	// 		if(this.x>650){
 	// 			this.x=-(this.line.width*3);
@@ -54,25 +56,26 @@ function Obj(x,y,line){
 }
 //Grounouille caractérisé par ses positions x,y son état(utilisé pour le bloqué) 
 //et son degré(utilisé pour le faire tourner lors de l'affichage)		
-function Frog(){
+class Frog{
+	constructor(){
 	this.x=310; 
     this.y=380;
 	this.state=false;
-	this.deg=0;
+	this.deg=0;}
 	
-	this.block=function(){
+	block(){
 		this.state=false;	
 	}
 	
-	this.unBlock=function(){
+	unBlock(){
 		this.state=true;	
 	}
 	
-	this.isNotBlocked=function(){
+	isNotBlocked(){
 		return this.state;	
 	}
 	
-	this.goUp=function(){
+	goUp(){
 		if(this.state){
 			this.y = this.y > 20 ? this.y -30 : this.y ;
 			this.deg=0;
@@ -82,7 +85,7 @@ function Frog(){
 		}
 	}
 	
-	this.goDown=function(){
+	goDown(){
 		if(this.state){
 			this.x = this.x > 310  && this.y ==170 
 			? this.x - (this.x-10)%30 
@@ -111,7 +114,7 @@ function Frog(){
 		}
 	}
 	
-	this.goLeft=function(){
+	goLeft(){
 		if(this.state){
 			this.x = this.x >10 
 			? this.x - 30 
@@ -124,7 +127,7 @@ function Frog(){
 		}
 	}
 	
-	this.goRight=function(){
+	goRight(){
 		if(this.state){
 			this.x = this.x < 610 
 			? this.x +30 
@@ -137,14 +140,14 @@ function Frog(){
 		}
 	}	
 			
-	this.initPos=function(){
+	initPos(){
 			this.unBlock();
 			this.x=310;
 			this.y=380;
 			this.deg=0;
 	}	
 	
-	this.isOut=function(){
+	isOut(){
 		return this.x<=0 || this.x >= 620 ;
 		// if(this.x<=0 || this.x>=620){
 		// 	return true;
@@ -154,14 +157,14 @@ function Frog(){
 		// }
 	}
 	
-	this.move=function(speed,dir){
+	move(speed,dir){
 		(this.state) ? this.x += (speed*dir): this.x ;
 		// if(this.state==true){
 		// 	this.x=this.x+(speed*dir);
 		// }
 	}
 	
-	this.hide=function(){
+	hide(){
 		this.block();
 		this.x=-30;
 		this.y=-30;
@@ -169,18 +172,19 @@ function Frog(){
 }
 
 //Nénuphar caractérisé par ses positions x,y et son état(si il est utilisé ou pas)
-function WaterLily(x,y){
+class WaterLily{
+	constructor (x,y){
 	this.x=x; 
     this.y=y;
-	this.state=false;
+	this.state=false}
 	
 	//Fonction qui retourne l'état du nénuphar (si il est utilisé ou pas)		
-	this.isUsed=function(){
+	isUsed(){
 		return this.state;
 	}
 	
 	//Fonction	qui determine si le frog est à l'intérieur du nénuphar ou pas
-	this.contains=function(frog){
+	contains(frog){
 		return frog.x>=this.x && (frog.x+30)<=(this.x+86) && !this.isUsed()
 		// if(frog.x>=this.x && (frog.x+30)<=(this.x+86) && !this.isUsed()){
 		// 	return true;
@@ -190,10 +194,10 @@ function WaterLily(x,y){
 		// }
 	}
 	
-	this.setUsed=function(){
+	setUsed(){
 		this.state=true;	
 	}
-	this.setUnused=function(){
+	setUnused(){
 		this.state=false;	
 	}
 }
